483908eb18f16edd1661efc202707d39e236dcbf
iyanuloluwa-Miracle <iyanudina@gmail.com>
2023-08-10 01:42:19 -0700

    added some changes

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Linking, StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-web';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.body}>
+      <Text style={styles.wrapper}>Learning React Native</Text>
+      <Button title='click me' onPress={()=>{Linking.openURL('https://github.com/iyanuloluwa-Miracle?tab=repositories')}}></Button>  
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  body:{
+    flex:1,
+    backgroundColor: 'black',
+    alignItems:'center',
+    justifyContent: 'center'
   },
+ 
+  wrapper:{
+    color:'white',
+    fontSize:30,
+    fontStyle:'italic',
+    margin:20
+  }
 });
