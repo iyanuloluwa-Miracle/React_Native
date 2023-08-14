716af1eb939e5f985e2461b63931353365ca12a9
iyanuloluwa-Miracle <iyanudina@gmail.com>
2023-08-14 04:55:27 -0700

    completed List, ScrollView & RefreshControl

--- App.js
+++ App.js
@@ -1,77 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import React, {useState} from 'react'
-import { Linking, StyleSheet, Text, View } from 'react-native';
-import { Button } from 'react-native-web';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { Linking, StyleSheet, Text, View, ScrollView, RefreshControl } from "react-native";
 
 export default function App() {
-  const [name,setName] = useState('Style Test')
+  const [items, setItems] = useState([
+    { key: 1, item: "Item 1" },
+    { key: 2, item: "Item 2" },
+    { key: 3, item: "Item 3" },
+    { key: 4, item: "Item 4" },
+    { key: 5, item: "Item 5" },
+    { key: 6, item: "Item 6" },
+    { key: 7, item: "Item 7" },
+    { key: 8, item: "Item 8" },
+    { key: 9, item: "Item 9" },
+    { key: 10, item: "Item 10" },
+    { key: 11, item: "Item 11" },
+  ]);
 
-  
-  
-  const onClickHandler = ()=>{
-    setName('Style Test is Done')
+  const [Refreshing, setRefreshing] = useState(false)
+
+  const onRefresh = () => {
+    setRefreshing(true);
+    setItems([...items, {key: 69, item:'Item 69'}])
+    setRefreshing(false)
   }
 
-
-
-
   return (
-    <View style={styles.body}>
-      <View style={styles.view1}>
-        <Text style={styles.wrapper}>1</Text>
-
-      </View>
-
-      <View style={styles.view2}>
-        <Text style={styles.wrapper}>2</Text>
-
-      </View>
-
-
-      <View style={styles.view3}>
-        <Text style={styles.wrapper}>3</Text>
-
-      </View>
+    <ScrollView 
+      style={styles.body}
+      refreshControl={<RefreshControl
+      refreshing={Refreshing}
+      onRefresh={onRefresh}
+      colors={['#ff00ff']}
       
+         />}
+      >
+      {items.map((object) => {
+        return (
+          <View style={styles.item} key={object}>
+            <Text style={styles.text}>{object.item}</Text>
+          </View>
+        );
+      })}
+
       <StatusBar style="auto" />
-    </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  body:{
-    flex:1,
-    flexDirection:'column',
-    backgroundColor: 'brown',
-    alignItems:'stretch',
-    justifyContent: 'center',
-   
+  body: {
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: "white",
   },
-  view1:{
-    flex:1,
-    backgroundColor:'orange',
-    alignItems:'center',
-    justifyContent:'center'
+  item: {
+    margin: 10,
+    backgroundColor: "pink",
+    justifyContent: "center",
+    alignItems: "center",
   },
-  view2:{
-    flex:1,
-    backgroundColor:'violet',
-    alignItems:'center',
-    justifyContent:'center'
+
+  text: {
+    fontSize: 36,
+    color: "blue",
+    margin: 10,
+    fontStyle: "italic",
   },
-  view3:{
-    flex:1,
-    backgroundColor:'pink',
-    alignItems:'center',
-    justifyContent:'center'
-  },
- 
-  wrapper:{
-    color:'black',
-    fontSize:30,
-    fontStyle:'black',
-    margin:20
-  }, 
- 
-  
 });
