d52d09ba87ab9c80b4c3867c3099d7e10f4367bb
iyanuloluwa-Miracle <iyanudina@gmail.com>
2023-08-15 20:00:26 -0700

    added some changes

--- App.js
+++ App.js
@@ -1,50 +1,66 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
 import { Linking, StyleSheet, Text, View, ScrollView, RefreshControl } from "react-native";
+import { FlatList } from "react-native-web";
 
 export default function App() {
-  const [items, setItems] = useState([
-    { key: 1, item: "Item 1" },
-    { key: 2, item: "Item 2" },
-    { key: 3, item: "Item 3" },
-    { key: 4, item: "Item 4" },
-    { key: 5, item: "Item 5" },
-    { key: 6, item: "Item 6" },
-    { key: 7, item: "Item 7" },
-    { key: 8, item: "Item 8" },
-    { key: 9, item: "Item 9" },
-    { key: 10, item: "Item 10" },
-    { key: 11, item: "Item 11" },
+  const [Items, setItems] = useState([
+    { name: "Item 1" },
+    { name: "Item 2" },
+    { name: "Item 3" },
+    { name: "Item 4" },
+    { name: "Item 5" },
+    { name: "Item 6" },
+    { name: "Item 7" },
+    { name: "Item 8" },
+    { name: "Item 9" },
+    { name: "Item 10" },
+    { name: "Item 11" },
   ]);
 
   const [Refreshing, setRefreshing] = useState(false)
 
   const onRefresh = () => {
     setRefreshing(true);
-    setItems([...items, {key: 69, item:'Item 69'}])
-    setRefreshing(false)
+    setItems([...Items, {key:69, name:'Item 69'}]); // Fixed the variable name
+    setRefreshing(false);
   }
+  
 
   return (
-    <ScrollView 
-      style={styles.body}
-      refreshControl={<RefreshControl
-      refreshing={Refreshing}
-      onRefresh={onRefresh}
-      colors={['#ff00ff']}
-      
-         />}
-      >
-      {items.map((object) => {
-        return (
-          <View style={styles.item} key={object}>
-            <Text style={styles.text}>{object.item}</Text>
-          </View>
-        );
-      })}
+    <FlatList
+    data={Items}
+    renderItem={({item})=> (
+      <View style={styles.item}>
+        <Text style={styles.text}>{item.name}</Text>
+      </View>
 
-      <StatusBar style="auto" />
-    </ScrollView>
+    )}
+    refreshControl={<RefreshControl
+        refreshing={Refreshing}
+        onRefresh={onRefresh}
+        colors={['#ff00ff']}
+    
+    />
+    // <ScrollView 
+    //   style={styles.body}
+    //   refreshControl={<RefreshControl
+    //   refreshing={Refreshing}
+    //   onRefresh={onRefresh}
+    //   colors={['#ff00ff']}
+      
+    //      />}
+    //   >
+    //   {items.map((object) => {
+    //     return (
+    //       <View style={styles.item} key={object}>
+    //         <Text style={styles.text}>{object.item}</Text>
+    //       </View>
+    //     );
+    //   })}
+
+    //   <StatusBar style="auto" />
+    // </ScrollView>
   );
 }
 
